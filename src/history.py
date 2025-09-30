d919919ce5a42004c34224bc145226a874e3bfd5
anil <anil.r.g12@gmail.com>
2025-09-30 23:30:18 +0900

    anil changed

--- src/history.py
+++ src/history.py
@@ -1,24 +1,36 @@
-
-### /src/history.py (starter)
+# src/history.py
 
 class BrowserHistory:
-    def __init__(self, start="home"):
-        # TODO: choose your internal representation (two stacks)
-        self._cur = start
-        self._back = []   # TODO
-        self._fwd = []    # TODO
+    def __init__(self):
+        self._back_stack = []
+        self._forward_stack = []
+        self._current = "home"
 
-    def visit(self, url: str) -> None:
-        # TODO: push current to back, set current, clear forward
-        raise NotImplementedError
+    def current(self):
+        return self._current
 
-    def back(self) -> str:
-        # TODO: move to previous page; decide error behavior on underflow
-        raise NotImplementedError
+    def visit(self, url: str):
+        # don't store the initial "home" in back history
+        if self._current != "home":
+            self._back_stack.append(self._current)
+        self._current = url
+        # visiting clears forward history
+        self._forward_stack.clear()
 
-    def forward(self) -> str:
-        # TODO: move to next page; decide error behavior on underflow
-        raise NotImplementedError
+    def back(self):
+        if not self._back_stack:
+            raise IndexError("No pages in back history")
+        # move current to forward stack
+        self._forward_stack.append(self._current)
+        # pop previous page as current
+        self._current = self._back_stack.pop()
+        return self._current
 
-    def current(self) -> str:
-        return self._cur
+    def forward(self):
+        if not self._forward_stack:
+            raise IndexError("No pages in forward history")
+        # don't push "home" into back history
+        if self._current != "home":
+            self._back_stack.append(self._current)
+        self._current = self._forward_stack.pop()
+        return self._current
